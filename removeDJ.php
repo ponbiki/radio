66f8207090e3c9ae180881269353f34af4266d88
ponbiki <jcandroscavage@hotmail.com>
2013-07-18 21:38:21 -0400

    Added removeDJ.php (need to finish deletion query)

--- removeDJ.php
+++ removeDJ.php
@@ -3,13 +3,11 @@ require 'header.php';
 
 if (!$loggedin) header("Location: index.php");
 ?>
-
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>DJ Panel</title>
+        <title>Remove DJ</title>
 
         <meta name="robots" content="noindex, nofollow" />
 
@@ -27,27 +25,29 @@ if (!$loggedin) header("Location: index.php");
         <div class="logo">
             <h1 title="7chan Radio">7chan Radio</h1>
         </div>
-        <div class="replymode">
-            <h2>DJ Panel</h2>
-        </div>
-        <p><span id="welcome">Welcome, <?php echo $user; ?>.</span></p>
-        <div class="dj">
-            <ul>
-                <li><a href="addDJ.php" title="Add DJ">Add New DJ</a></li>
-                <li><a href="removeDJ.php" title="Remove DJ">Remove DJ</a></li>
-                <li><a href="changepwd.php" title="Change Password">Change Password</a></li>
-            </ul>
-        </div>
-        <table>
-            <tr><th>DJs</th></tr>
-            <?php
-            $query = "SELECT * FROM djs";
-            $result = mysql_query($query);
-            $rows = mysql_num_rows($result);
+        <?php
+        $error = $user = $pass = "";
 
-            for ($j = 0; $j < $rows; ++$j) {
-                echo "<tr><td>" . mysql_result($result,$j,'username') . "</td></tr>";
-            }
-            ?>
-        </table>
+        $query1 = "SELECT * FROM djs";
+        $result1 = mysql_query($query1);
+        /*$rows1 = mysql_num_rows($result1);
+
+        for ($j = 0; $j < $rows1; ++$j) {
+            "<option value=" . mysql_result($result1,$j,'username') . ">" . mysql_result($result1,$j,'username') . "</option>";
+        }*/
+        ?>
+        <div class="replymode">
+            <h2>Remove DJ</h2>
+        </div>
+        <form method='post' action='removeDJ.php'><?php echo $error; ?>
+            DJ &nbsp; &nbsp;<select name='DJ' size='1'>
+                <?php while($row = mysql_fetch_array($result1)) {
+                    echo "<option value='" . $row[username] . "'>" . $row[username] . "</option>";
+                } ?>
+            </select><br /><br />
+            <label>Confirm Selection &nbsp; <input type="checkbox" name="confirm"></label><br />
+            &nbsp; &nbsp;
+            <input type='submit' value='Remove DJ' />
+        </form>
+    </body>
 </html>
