e995ab24042420ab536abb9eeda99c3b73ff3060
ponbiki <jcandroscavage@hotmail.com>
2013-10-06 12:55:36 -0400

    adding schedule function (needs debugged)

--- schedule.php
+++ schedule.php
@@ -53,4 +53,9 @@ $firstDayArray = getdate($start);
                 }
                 ?>
             </select>
+            <select name="year">
+                <?php
+                
+                ?>
+            </select>
         </form>
