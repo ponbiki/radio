a52ba1cd99f8c979c1264314424ed89760ea89c3
ponbiki <jcandroscavage@hotmail.com>
2014-02-23 06:26:21 -0500

    changed comparison arguments in radiograb.php to reflect broader user input corrected array in nowplaying.php

--- radiograb.php
+++ radiograb.php
@@ -17,4 +17,20 @@ $cleaned = strip_tags($out);
 $stripped = preg_replace("/\s+/", " ", $cleaned);
 $split = explode(" ", $stripped);
 echo "<pre>";print_r($split);echo "</pre>";
+//$radio_on = in_array('/radio', $split);
+//if($radio_on) {echo "lol";} else {echo "nope";}
+//echo current($split);
+if (in_array('/radio', $split)) {
+    while (current($split) != '/radio') {
+        next($split);
+        $radio_key = key($split);
+        $dj_name = $split[$radio_key + 4];
+        $stream_type = $split[$radio_key + 11];
+        $bit_rate = $split[$radio_key + 21];
+        $current_listeners = $split[$radio_key + 24];
+        $peak_listeners = $split[$radio_key + 27];
+    }
+}
+echo $dj_name;
+echo $stream_type;
 ?>
